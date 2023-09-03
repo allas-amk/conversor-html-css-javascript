886f0d8205ada0e115427a03d0c2ac6714acac8f
Allas Maycon <51690108+allas-amk@users.noreply.github.com>
2023-09-03 13:06:00 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+
+const form = document.getElementById('form');
+form.addEventListener('')
+
+const inputValue = document.getElementById('value-real');
+const selectedCurrency = document.getElementById('currency');
+const result = document.getElementById('result');
